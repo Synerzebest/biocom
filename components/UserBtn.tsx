3c7e59316126fa17a0b0c38f8437f9322f82e375
Charles Guelton <charlesguelton07@gmail.com>
2024-03-23 22:19:36 +0100

    positionning user button

--- components/UserBtn.tsx
+++ components/UserBtn.tsx
@@ -3,8 +3,8 @@ import { UserButton } from '@clerk/nextjs';
 
 export default function UserBtn() {
     return (
-        <div className="fixed top-[49rem] left-[1rem] z-10 flex gap-4 p-[10px] shadow-lg bg-white items-center rounded-lg">
-            <p className="text-xl">Moi</p>
+        <div className="fixed bottom-2 left-2 z-10 flex gap-4 p-[10px] shadow-lg bg-white items-center rounded-lg">
+            <p className="text-xl">Me</p>
             <UserButton />
         </div>
     )
